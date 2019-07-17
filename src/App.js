b49e441554f0bf2b7878c076c7bc98afc99efdd5
Samrat Saurav Jaiswal <coolsaurav34@gmail.com>
2019-07-17 21:12:38 +0530

    hook workflow

--- src/App.js
+++ src/App.js
@@ -1,44 +1,41 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  
-  state = {
+const app = props => {
+  const [personsState, setPersonState] = useState({
     persons : [
       {name: 'Samrat', age : 20},
       {name: 'Aditya', age : 21},
       {name: 'Shubham', age : 22},
     ]
-  }
-  
-  switchNameHandler = () =>{
+  });
+
+  const switchNameHandler = () => {
     // console.log("cool");
     //DONT DO THIS this.state.persons[0].name = 'urdaddy';  
-    this.setState({
+    setPersonState({
       persons : [
         {name: 'Samrat Saurav Jaiswal', age: 21},
         {name: 'Aditya Kumar Jha', age: 21},
         {name: 'Shubham', age: 22}
       ]
     })
-  }
+  } 
 
-  render() {
     return (
       <div className="App">
       <h1>Hi, I'm the BEST...</h1>
       <h2>I'm changing the world for better.</h2>
       <p>Creating new <b>TECHNOLOGY</b> for peace and earth friendly.</p>
-      <button onClick={this.switchNameHandler}>Switch Name</button>
-      <Person name={this.state.persons[0].name} age= {this.state.persons[0].age} > »» Samrat «« </Person>
-      <Person name={this.state.persons[1].name} age= {this.state.persons[1].age} > »» Mr.Encyclopedia «« </Person>
-      <Person name={this.state.persons[2].name} age= {this.state.persons[2].age} > »» Mahadeva «« </Person>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age= {personsState.persons[0].age} > »» Samrat «« </Person>
+      <Person name={personsState.persons[1].name} age= {personsState.persons[1].age} > »» Mr.Encyclopedia «« </Person>
+      <Person name={personsState.persons[2].name} age= {personsState.persons[2].age} > »» Mahadeva «« </Person>
       </div>
     );
     // return React.createElement('div', {className:"App"}, React.createElement('h1', null, 'I\'m the BEST...') ,
     //  React.createElement('p', 'null', 'cool' ))
-  }
 }
 
-export default App;
+export default app;
